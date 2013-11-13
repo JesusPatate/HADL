1bd2f33f8c7fdcf545430a4509f6076633278881
ggautier <gaetan.gautier@etu.univ-angers.fr>
2013-11-13 18:11:36 +0100

    connecteur SQLQuery

---
+++ hadl/src/hadl/m1/serverDetails/SQLQuery.java
@@ -0,0 +1,64 @@
+package hadl.m1.serverDetails;
+
+import hadl.m1.Call;
+import hadl.m2.Message;
+import hadl.m2.connector.AtomicConnector;
+import hadl.m2.connector.Role;
+
+public class SQLQuery extends AtomicConnector {
+    
+    class SQLQuerySender extends Role {
+        
+        public SQLQuerySender() {
+            super("sender");
+        }
+        
+        @Override
+        public void receive(Message msg) {
+            Call call = (Call) msg;
+            String service = call.getCalledService();
+            
+            if(service.equals("receiveDBResponse")) {
+                if(this.attachment != null) {
+                    this.attachment.send(this, msg);
+                }
+            }
+            else {
+                receiverRole.receive(msg);
+            }
+        }
+    }
+    
+    class SQLQueryReceiver extends Role {
+        
+        public SQLQueryReceiver() {
+            super("receiver");
+        }
+        
+        @Override
+        public void receive(Message msg) {
+            Call call = (Call) msg;
+            String service = call.getCalledService();
+            
+            if(service.equals("receiveDBResponse")) {
+                SQLQuery.this.senderRole.receive(msg);
+            }
+            
+            else if (this.attachment != null) {
+                this.attachment.send(this, msg);
+            }
+        }
+    }
+    
+    private final SQLQuerySender senderRole =
+            new SQLQuerySender();
+    
+    private final SQLQueryReceiver receiverRole =
+            new SQLQueryReceiver();
+    
+    public SQLQuery(String label) {
+        super(label);
+        this.addRole(this.senderRole);
+        this.addRole(this.receiverRole);
+    }
+}
