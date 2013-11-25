83573fa3a255b8f27bcd0832274657359bfc4a8a
Stanislas KOBAK <E071318U@S046V2pc15.ensinfo.sciences.univ-nantes.prive>
2013-11-25 14:48:13 +0100

    clearance query + database ok

--- fr.univnantes.alma.hadl.m1/src/fr/univnantes/alma/hadl/m1/serverDetails/ClearanceQuery.java
+++ fr.univnantes.alma.hadl.m1/src/fr/univnantes/alma/hadl/m1/serverDetails/ClearanceQuery.java
@@ -1,16 +1,45 @@
 package fr.univnantes.alma.hadl.m1.serverDetails;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import fr.univnantes.alma.hadl.m2.connector.AtomicConnector;
 import fr.univnantes.alma.hadl.m2.connector.Role;
+import fr.univnantes.alma.hadl.m2.service.IncompatibleServiceException;
+import fr.univnantes.alma.hadl.m2.service.NotConnectedServiceException;
+import fr.univnantes.alma.hadl.m2.service.Service;
 
 
-public class ClearanceQuery extends AtomicConnector {    
+public class ClearanceQuery extends AtomicConnector { 
+	
+	private static final Map<String, Class<?>> SECURITY_AUTHORIZATION_PARAMS =
+            new HashMap<String, Class<?>>();
+    
+    static {
+    	 SECURITY_AUTHORIZATION_PARAMS.put("login", String.class);
+         SECURITY_AUTHORIZATION_PARAMS.put("password", String.class);
+    }
+	
+	private class SecurityAuthorization extends Service {
+		SecurityAuthorization() {
+			super("clearanceRequest", boolean.class, SECURITY_AUTHORIZATION_PARAMS);
+		}
+    }
+	
     public ClearanceQuery(String label) {
         super(label);
-        Role senderRole = new Role("sender");
-        Role receiverRole = new Role("receiver");
+        Role requestor = new Role("requestor");
+        Role grantor = new Role("grantor");
+        SecurityAuthorization required = new SecurityAuthorization();
+        SecurityAuthorization provided = new SecurityAuthorization();
         
-        addRole(senderRole);
-        addRole(receiverRole);
+        addProvidedService(grantor, provided);
+        try {
+        	addRequiredService(requestor, required, provided);
+		} catch (NotConnectedServiceException e) {
+			e.printStackTrace();
+		} catch (IncompatibleServiceException e) {
+			e.printStackTrace();
+		}
     }
 }
--- fr.univnantes.alma.hadl.m1/src/fr/univnantes/alma/hadl/m1/serverDetails/Database.java
+++ fr.univnantes.alma.hadl.m1/src/fr/univnantes/alma/hadl/m1/serverDetails/Database.java
@@ -1,8 +1,13 @@
 package fr.univnantes.alma.hadl.m1.serverDetails;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
+import fr.univnantes.alma.hadl.m1.DBRequest;
+import fr.univnantes.alma.hadl.m1.cs.DBResponse;
+import fr.univnantes.alma.hadl.m2.Request;
 import fr.univnantes.alma.hadl.m2.Response;
 import fr.univnantes.alma.hadl.m2.component.AtomicComponent;
 import fr.univnantes.alma.hadl.m2.component.Port;
@@ -11,24 +16,55 @@ import fr.univnantes.alma.hadl.m2.service.Service;
 
 
 public class Database extends AtomicComponent {
+	
+	private static final Map<String, Class<?>> SECURITY_MANAGEMENT_PARAMS =
+            new HashMap<String, Class<?>>();
+	
+	private static final Map<String, Class<?>> HANDLE_QUERY_PARAMS =
+            new HashMap<String, Class<?>>();
     
-    private class SecurityManagementService extends Service {
-        public SecurityManagementService() {
-        	// TODO: signature à compléter
-            super("securityManagement", null, null);
+    static {
+        SECURITY_MANAGEMENT_PARAMS.put("login", String.class);
+        SECURITY_MANAGEMENT_PARAMS.put("password", String.class);
+        HANDLE_QUERY_PARAMS.put("request", DBRequest.class);
+    }
+    
+    private class SecurityManagement extends Service {
+    	
+        public SecurityManagement() {
+            super("securityManagement", boolean.class, SECURITY_MANAGEMENT_PARAMS);
         }
     }
     
-    private class QueryService extends ProvidedService {
-        public QueryService() {
-        	// TODO: signature à compléter
-            super("manageUsers", null, null);
+    private class HandleQuery extends ProvidedService {
+        public HandleQuery() {
+            super("handleQuery", DBResponse.class, HANDLE_QUERY_PARAMS);
         }
 
 		@Override
 		public Response excecute(Map<String, Object> parameters) {
-			// TODO Auto-generated method stub
-			return null;
+			Response resp = null;
+			Map<String, Object> authParameters = new HashMap<String, Object>();
+			authParameters.put("login", parameters.get("login"));
+			authParameters.put("password", parameters.get("password"));
+			Request authRequest = new Request("securityManagement", authParameters);
+			resp = send(authRequest);
+			boolean authorized = (Boolean) resp.getValue();
+			
+			if(authorized){
+				List<Object> values = new LinkedList<Object>();
+				values.add("Sucess !!!");
+				DBResponse dbResp = new DBResponse(values);
+				resp = new Response(dbResp);
+			}
+			else{
+				List<Object> values = new LinkedList<Object>();
+				values.add("Not authorized to access to databse");
+				DBResponse dbResp = new DBResponse(values, true);
+				resp = new Response(dbResp);
+			}
+			
+			return resp;
 		}
     }
     
@@ -39,8 +75,8 @@ public class Database extends AtomicComponent {
         Port securityManagement = new Port("securityManagement");
         Port query = new Port("query");
         
-        addRequiredConnection(securityManagement, new SecurityManagementService());
-        addProvidedConnection(query, new QueryService());
+        addRequiredConnection(securityManagement, new SecurityManagement());
+        addProvidedConnection(query, new HandleQuery());
     }
     
     /*private boolean addUser(final String login, final String pwd) {
